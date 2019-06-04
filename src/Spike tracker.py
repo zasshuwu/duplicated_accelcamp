eaffe84607d6cf451f5831616ec78d4bcd057d0d
Quikol <chan.jch19@gmail.com>
2019-06-04 13:58:37 -0400

    Spikes first version
    Might be missing omega

--- src/Spike tracker.py
+++ src/Spike tracker.py
@@ -1,43 +1,43 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 from MyFunctions import *
+from Load import *
 
-fullPathName =
-def LoadSpike(fullPathName):
-    if(filepath == None):
-        #File Dialog Options
-        myOpts = {}
-        myOpts['initialfile'] = 'TopRight_Feb18.csv'
-        filepath = dialogOpenFilename(myOpts)
-    # unpack makes it column-major
-    block =np.loadtxt( fullPathName,dtype=float, comments= ";", delimiter=',', usecols=(0,1,2,3), unpack=True,
-                       skiprows=10)
-    a= block[0:] #a[0] is time
-    return a
+#fullPathName = 'C:\\Users\\Jeff\Documents\\GitHub\\2019_06_AccelerationCamp\\data\\Spike\\DATA-017.csv'
+r=LoadRun()
+a= r["accel"][0].a #a[0] is time
+time= r["accel"][0].t
+ax = a[0]
+ay= a[1]
 
-def Spike():
-    plt.subplot(2,1,1)
-    plt.plot(t,ax, label="Accel in x")
-    plt.minorticks_on()
-    plt.grid(b=True, which='both', color='0.65',linestyle='-')
-    plt.xlabel("Time")
-    plt.ylabel("Acceleration in x-direction")
-    '''ax_mean = [np.mean(ax)]*len(time)
-    ax_mean_line = plt.plot(time,ax_mean, label='Mean', linestyle='--')
-    plt.legend(['Acceleration in x', 'Average in x'])
-    plt.subplot(2,1,2)
-    plt.subplots_adjust(hspace=0.3)
-    plt.plot(time,ay, label='Accel in y')
-    plt.minorticks_on()
-    plt.grid(b=True, which='both', color='0.65',linestyle='-')
-    plt.xlabel("Time")
-    plt.ylabel("Acceleration in y-direction")
-    ay_mean = [np.mean(ay)]*len(time)
-    ay_mean_line = plt.plot(time,ay_mean, label='Mean', linestyle='--')
-    plt.legend(['Acceleration in y', 'Average in y'])'''
-    plt.ion()
-    plt.show()
+abs_x= np.absolute(ax)
+abs_y= np.absolute(ay)
 
-LoadSpike()
-Spike()
+#time_x = np.argmax(abs_y)]
+print(np.amax(abs_x))
+print(np.amax(abs_y))
+print(time[np.argmax(abs_x)])
+print(time[np.argmax(abs_y)])
+
+plt.subplot(2, 1, 1)
+plt.plot(time,ax, label="Accel in x")
+plt.minorticks_on()
+plt.grid(b=True, which='both', color='0.65',linestyle='-')
+plt.xlabel("Time")
+plt.ylabel("Acceleration in x-direction")
+ax_mean = [np.mean(ax)]*len(time)
+ax_mean_line = plt.plot(time,ax_mean, label='Mean', linestyle='--')
+plt.legend(['Acceleration in x', 'Average in x'])
+plt.subplot(2,1,2)
+plt.subplots_adjust(hspace=0.3)
+plt.plot(time,ay, label='Accel in y')
+plt.minorticks_on()
+plt.grid(b=True, which='both', color='0.65',linestyle='-')
+plt.xlabel("Time")
+plt.ylabel("Acceleration in y-direction")
+ay_mean = [np.mean(ay)]*len(time)
+ay_mean_line = plt.plot(time,ay_mean, label='Mean', linestyle='--')
+plt.legend(['Acceleration in y', 'Average in y'])
+#plt.pause(10)
+plt.show()
