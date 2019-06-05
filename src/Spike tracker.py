073614ce9f11f527b8e2498b5cf090e17e2ad1cb
Quikol <chan.jch19@gmail.com>
2019-06-05 15:03:44 -0400

    End of Day 3

--- src/Spike tracker.py
+++ src/Spike tracker.py
@@ -5,39 +5,43 @@ from MyFunctions import *
 from Load import *
 
 #fullPathName = 'C:\\Users\\Jeff\Documents\\GitHub\\2019_06_AccelerationCamp\\data\\Spike\\DATA-017.csv'
-r=LoadRun()
-a= r["accel"][0].a #a[0] is time
-time= r["accel"][0].t
-ax = a[0]
-ay= a[1]
+r=Load_X2()
+a= r.a #a[0] is time
+time= r.t
 
-abs_x= np.absolute(ax)
-abs_y= np.absolute(ay)
+time2 = list(filter(lambda x : x < 8, list(time)))
+ax = a[0][:len(time2)]
+ay= a[1][:len(time2)]
+
+mag = np.sqrt(np.square(ax)+np.square(ay))
+
+abs_mag= np.absolute(ax)
+#abs_y= np.absolute(ay)
 
 #time_x = np.argmax(abs_y)]
-print(np.amax(abs_x))
-print(np.amax(abs_y))
-print(time[np.argmax(abs_x)])
-print(time[np.argmax(abs_y)])
+print(np.amax(abs_mag))
+#print(np.amax(abs_y))
+print("Time of max a " + str(time[np.argmax(abs_mag)]))
+#print("Time of max " + str(time[np.argmax(abs_y)]))
 
 plt.subplot(2, 1, 1)
-plt.plot(time,ax, label="Accel in x")
+plt.plot(time2,mag, label="Accel in x")
 plt.minorticks_on()
 plt.grid(b=True, which='both', color='0.65',linestyle='-')
 plt.xlabel("Time")
 plt.ylabel("Acceleration in x-direction")
-ax_mean = [np.mean(ax)]*len(time)
-ax_mean_line = plt.plot(time,ax_mean, label='Mean', linestyle='--')
+ax_mean = [np.mean(mag)]*len(time2)
+ax_mean_line = plt.plot(time2,ax_mean, label='Mean', linestyle='--')
 plt.legend(['Acceleration in x', 'Average in x'])
-plt.subplot(2,1,2)
+
+'''plt.subplot(2,1,2)
 plt.subplots_adjust(hspace=0.3)
-plt.plot(time,ay, label='Accel in y')
+plt.plot(time2,ay, label='Accel in y')
 plt.minorticks_on()
 plt.grid(b=True, which='both', color='0.65',linestyle='-')
 plt.xlabel("Time")
 plt.ylabel("Acceleration in y-direction")
-ay_mean = [np.mean(ay)]*len(time)
-ay_mean_line = plt.plot(time,ay_mean, label='Mean', linestyle='--')
-plt.legend(['Acceleration in y', 'Average in y'])
-#plt.pause(10)
+ay_mean = [np.mean(ay)]*len(time2)
+ay_mean_line = plt.plot(time2,ay_mean, label='Mean', linestyle='--')
+plt.legend(['Acceleration in y', 'Average in y'])'''
 plt.show()
