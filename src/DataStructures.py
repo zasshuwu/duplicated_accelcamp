d1de0889df08d19e17ad4fb6f77dff3b16a435e2
Chabbert <1854931@johnabbottcollege.net>
2019-06-11 11:43:04 -0400

    Vec3 Update
    + Added a function that refreshes AccelData.vec3

--- src/DataStructures.py
+++ src/DataStructures.py
@@ -5,9 +5,12 @@ class AccelData:
         self.a = a
         self.t = t
         self.len = len(self.a[0])
+        self.GenVec3()
+
+    def GenVec3(self):
         self.vec3 = []
-        for i in range(len(a[0])):
-            self.vec3.append(np.array([a[0][i], a[1][i], a[2][i]]))
+        for i in range(len(self.a[0])):
+            self.vec3.append(np.array([self.a[0][i], self.a[1][i], self.a[2][i]]))
         self.vec3 = np.array(self.vec3)
 
 class RotaryData:
