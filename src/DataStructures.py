3e9cb0b15a7eda09c13baea7b95ac0aa7d5a5ee9
Chabbert <1854931@johnabbottcollege.net>
2019-06-11 11:32:35 -0400

    Update
    + New AccelData.vec3
    - Removed AccelData.samplerate
    + Added config section to tfAccelOptimizer
    + Added use_synthetic_data option

--- src/DataStructures.py
+++ src/DataStructures.py
@@ -1,12 +1,17 @@
+import numpy as np
+
 class AccelData:
-    def __init__(self,a,t,sr):
+    def __init__(self,a,t):
         self.a = a
         self.t = t
-        self.samplerate = sr
         self.len = len(self.a[0])
+        self.vec3 = []
+        for i in range(len(a[0])):
+            self.vec3.append(np.array([a[0][i], a[1][i], a[2][i]]))
+        self.vec3 = np.array(self.vec3)
 
 class RotaryData:
-    def __init__(self,omega,t,sr):
+    def __init__(self,omega,t):
         self.omega = omega
         self.t = t
-        self.samplerate = sr
+        self.len = len(self.omega)
