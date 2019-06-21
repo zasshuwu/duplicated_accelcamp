365cb865bc5b29bb222d41a4483664a956c38f52
Guillaume Alain <gyomalin@gmail.com>
2019-06-21 15:36:53 -0400

    adding tensorflow unit test

--- src/tf/test_hard_stuff.py
+++ src/tf/test_hard_stuff.py
@@ -1,6 +1,28 @@
 import unittest
+import numpy as np
+import tensorflow as tf
 
-class TestStringMethods(unittest.TestCase):
+# (Guillaume) This doesn't want to cooperate, and I'm not sure about the easiest way to resolve this.
+# One option would be to include things in the PYTHONPATH environment variable,
+# but it would be a messy solution. Hmmm ...
+#
+# from ./tfPhysics import tfSessionInitRun
 
-    def test_multiply(self):
-        self.assertEqual(3*4, 12)
+
+class TestTensorflowOperations(unittest.TestCase):
+
+    def test_adding_vectors(self):
+        vec3 = np.array([1.0,0.0,0.0])
+        reference_value = vec3 + vec3
+
+        ph_vec3 = tf.placeholder(tf.float32, vec3.shape)
+        my_sum = ph_vec3 + ph_vec3
+        my_feed_dict = {ph_vec3: vec3}
+
+        sess = tf.Session() # tfSessionInitRun()
+        experimental_value = sess.run(my_sum, feed_dict=my_feed_dict)
+
+        # this is probably not how you're supposed to compare two arrays,
+        # but right now I don't see the obvious thing, or whether it would
+        # be okay to use numpy.testing and it's operations like "assertAlmostAllEqual"
+        self.assertEqual(0.0, np.max(np.abs(experimental_value - reference_value)))
