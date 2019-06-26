55e9fea997ba93a386ab6fc0f80bc723b5f6affd
Chabje1 <chabje01@gmail.com>
2019-06-26 12:08:41 -0400

    Updated Tests to be Unittest-friendly

--- src/TestLoadPlot.pyw
+++ src/TestLoadPlot.pyw
@@ -4,5 +4,5 @@ from Load import *
 from MyFunctions import *
 from Plotter import *
 
-run = LoadRun()
+run = LoadRun(None if __name__ == "__main__" else "../data/2019 06 12/0 degrees/run1/")
 Plot(run["accel"],  run["omega"])
--- src/Test_Curvature.py
+++ src/Test_Curvature.py
@@ -5,24 +5,28 @@ from Simulate import *
 import matplotlib.pyplot as plt
 
 _range = [13, 18]
+if __name__ == "__main__":
+    use_range = True if input("use time range? (y/n): ") == "y" else False
+    if use_range:
+        _range = [float(input("Beginning: ")), float(input("End: "))]
 
-use_range = True if input("use time range? (y/n): ") == "y" else False
-if use_range:
-    _range = [float(input("Beginning: ")), float(input("End: "))]
-
-if True if input("use synthetic data? (y/n): ") == "y" else False:
-    if True if input("use omega file? (y/n): ") == "y" else False:
-        o = LoadRun()["omega"][0]
+    if True if input("use synthetic data? (y/n): ") == "y" else False:
+        if True if input("use omega file? (y/n): ") == "y" else False:
+            o = LoadRun()["omega"][0]
+        else:
+            o = simConstAlpha(
+                int(input("Number of iterations: ")),
+                np.float32(input("Delta t: ")),
+                np.float32(input("alpha: ")),
+                np.float32(input("omega at t=0: "))
+            )
+        acceldat = convertOmegaAccel(o, np.float32(input("Radius: ")))
     else:
-        o = simConstAlpha(
-            int(input("Number of iterations: ")),
-            np.float32(input("Delta t: ")),
-            np.float32(input("alpha: ")),
-            np.float32(input("omega at t=0: "))
-        )
-    acceldat = convertOmegaAccel(o, np.float32(input("Radius: ")))
+        acceldat = LoadRun()["accel"][0]
+
 else:
-    acceldat = LoadRun()["accel"][0]
+    use_range = False
+    acceldat = LoadAccelFile("../data/2019 06 12/0 degrees/run1/run1.accel.x2.CSV")
 
 if use_range:
     mask = np.logical_not((_range[0] <= acceldat.t[:-1]) ^ (_range[1] >= acceldat.t[:-1]))
