6efc849b36b3389fe67f59ede389eb3f37d7a503
Chabje1 <chabje01@gmail.com>
2020-01-16 11:17:52 -0500

    SpikeTracker Update
    Load.py : Now Raises Error if no files were found
    
    SpikeTracker.py : Works with the new data structure and trims data
    
    Test_SpikePlot.py : Removed the trimming part. Now looks more like a test file.

--- src/SpikeTracker.py
+++ src/SpikeTracker.py
@@ -1,106 +1,131 @@
 import numpy as np
-from DataStructures import *
+from DataStructuresNew import *
+from scipy.signal import resample
+from MyFunctions import min_lambda
 
 adjust = 0.5
 
+
 def SpikeAdjust(_AccelDatas, _RotaryDatas):
+    """ shift time axis of each data set so that the spikes are aligned.
+    Does not trim the length of the data sets"""
     global adjust
     AccelDatas = _AccelDatas
     RotaryDatas = _RotaryDatas
 
-    time_list = []
+    time_list = {}
     min_t_list = None
     min_t_list_spike = None
+
     '''================Acceleration================='''
     for ad in AccelDatas:
-        a = ad.a  # a[0] is time
-        time1 = ad.t
-        time1 = list(filter(lambda x: x < 7, list(time1)))
-        ax = a[0][:len(time1)]
-        ay = a[1][:len(time1)]
+        spike_time = FindSpike(ad)
 
-        mag = np.sqrt(np.square(ax) + np.square(ay))
+        time_list[ad] = spike_time
+        # time_list is a dictionary of all timestamps for the spikes
 
-        abs_mag = np.absolute(mag)
-        time_list.append(time1[np.argmax(abs_mag)])
-        if min_t_list is None:
-            min_t_list = ad.t
-            min_t_list_spike = time1[np.argmax(abs_mag)]
-        elif len(min_t_list) > len(ad.t):
-            min_t_list = ad.t
-            min_t_list_spike = time1[np.argmax(abs_mag)]
+        if min_t_list is None:  # check if we already have a minimum
+            min_t_list = ad.t   # set the minimum time to this AccelData's time
+            min_t_list_spike = spike_time   # set the spike timestamp
+
+        elif len(min_t_list) > len(ad.t):  # check to see if our list is shorter than the previous minimum's
+            min_t_list = ad.t   # set the minimum time to this AccelData's time
+            min_t_list_spike = spike_time   # set the spike timestamp
 
     '''================Omega================='''
     for od in RotaryDatas:
-        b = od.omega
-        time2 = od.t
-        time2 = list(filter(lambda x: x < 7, list(time2)))
+        spike_time = FindSpike(od)
 
-        omega = b[:len(time2)]
-        abs_omega = np.absolute(omega)
-        time_list.append(time2[np.argmax(abs_omega)])
-        if min_t_list is None:
-            min_t_list = od.t
-            min_t_list_spike = time2[np.argmax(abs_omega)]
-        elif len(min_t_list) > len(od.t):
-            min_t_list = od.t
-            min_t_list_spike = time2[np.argmax(abs_omega)]
+        time_list[od] = spike_time
+        # time_list is a dictionary of all timestamps for the spikes
+
+        if min_t_list is None:  # check if we already have a minimum
+            min_t_list = od.t  # set the minimum time to this RotaryData's time
+            min_t_list_spike = spike_time  # set the spike timestamp
+
+        elif len(min_t_list) > len(od.t):  # check to see if our list is shorter than the previous minimum's
+            min_t_list = od.t  # set the minimum time to this RotaryData's time
+            min_t_list_spike = spike_time  # set the spike timestamp
 
     '''================Process================='''
-    time_list.sort()
-    min_val = time_list[0]
+    time3 = list(time_list.values())
+    time3.sort()
+    min_val = time3[0]
+
     adjust = min_val - adjust
     min_t_list -= (min_t_list_spike - min_val) + adjust
+
     for i in range(len(AccelDatas)):
         ad = AccelDatas[i]
-        a = ad.a  # a[0] is time
-        time1 = ad.t
-        time1 = list(filter(lambda x: x < 7, list(time1)))
-        ax = a[0][:len(time1)]
-        ay = a[1][:len(time1)]
 
-        mag = np.sqrt(np.square(ax) + np.square(ay))
+        spike_time = time_list[ad]
 
-        abs_mag = np.absolute(mag)
-        delta = time1[np.argmax(abs_mag)] - min_val
+        delta = spike_time - min_val
         time1 = ad.t - (delta + adjust)
         z = len(list(filter(lambda x: x < 0, time1)))
+
         AccelDatas[i].t = np.array(list(filter(lambda x: x >= 0, time1)))
-        AccelDatas[i].a = np.array([
-            a[0][z:],
-            a[1][z:],
-            ad.a[2]
-        ])
+        AccelDatas[i].a = ad.a[z:]
 
     for i in range(len(RotaryDatas)):
         od = RotaryDatas[i]
-        b = od.omega
-        time2 = od.t
-        time2 = list(filter(lambda x: x < 7, list(time2)))
 
-        omega = b[:len(time2)]
-        abs_omega = np.absolute(omega)
-        delta = time2[np.argmax(abs_omega)] - min_val
-        time2 -= delta + adjust
+        spike_time = time_list[od]
+
+        delta = spike_time - min_val
+        time2 = od.t - (delta + adjust)
         z = len(list(filter(lambda x: x < 0, time2)))
         RotaryDatas[i].t = np.array(list(filter(lambda x: x >= 0, time2)))
-        RotaryDatas[i].omega = b[z:]
+        RotaryDatas[i].omega = od.omega[z:]
 
-    return AccelDatas, RotaryDatas
+    return TrimSets(AccelDatas, RotaryDatas)
+
+
 
 def FindSpike(_dataset):
-    data = _dataset.a if type(_dataset) == Acceldata else _dataset.o
+    """ Only adjust the x & y axes
+    TODO: smooth the data before finding the max magnitude?"""
+    if type(_dataset) == AccelData:
+        mag = np.sqrt(np.square(_dataset.getSingleAxis(0)) + np.square(_dataset.getSingleAxis(1)))
+
+    elif type(_dataset) == RotaryData:
+        mag = _dataset.omega
+
+    else:
+        raise ValueError('Unknown Dataset Type: ' + str(type(_dataset)))
+
     time_series = list(filter(lambda x: x < 7, list(_dataset.t)))
+    # Here
+    #  - 'lambda x: x < 7' = a function which returns the result of x < 7 for any x
+    #      - this is the key in filter(key, arr) meaning that function will be applied to all elements in arr
+    #  - filter( ) removes all values in an array that have returned false for the lambda expression
+    #  - list( ) converts the object into an array
 
-    magsqred = np.array(len(time_series))
-    for axisdata in data:
-        magsqred += np.square(axisdata[:len(time_series)])
-
-    mag = np.sqrt(magsqred)
-
-    abs_mag = np.absolute(mag)
+    abs_mag = np.absolute(mag[:len(time_series)])
     spike_t = time_series[np.argmax(abs_mag)]
     return spike_t
 
-def AdjustToSpike(_dataset, _spike):
-    return
+
+def TrimSets(ad_accel, ad_omega):
+    """ Trims all data sets from SpikeAdjust to match the smallest one """
+    min_dat = min_lambda(len, [
+        min_lambda(len, ad_omega),
+        min_lambda(len, ad_accel)
+    ])
+
+    for i in range(len(ad_accel)):
+        ad_accel[i].a = np.array(
+            [
+                resample(ad_accel[i].getSingleAxis(0), len(min_dat.t)),
+                resample(ad_accel[i].getSingleAxis(1), len(min_dat.t)),
+                resample(ad_accel[i].getSingleAxis(2), len(min_dat.t))
+            ]
+        ).transpose()
+
+        ad_accel[i].t = min_dat.t
+
+    for i in range(len(ad_omega)):
+        ad_omega[i].omega = np.array(resample(ad_omega[i].omega, len(min_dat.t)))
+        ad_omega[i].t = min_dat.t
+
+    return ad_accel, ad_omega
--- src/Test_SpikePlot.py
+++ src/Test_SpikePlot.py
@@ -1,8 +1,6 @@
 from Load import *
 from SpikeTracker import *
 from Plotter import *
-from scipy.signal import resample
-from MyFunctions import min_lambda
 
 from unittest import *
 
@@ -20,24 +18,5 @@ else:
 
 
 ad_accel, ad_omega = SpikeAdjust(ad_accel, ad_omega)
-
-min_dat = min_lambda(len, [
-    min_lambda(len, ad_omega),
-    min_lambda(len, ad_accel)
-])
-
-for i in range(len(ad_accel)):
-    ad_accel[i].a = np.array(
-        [
-        resample(ad_accel[i].a[0], len(min_dat.t)),
-        resample(ad_accel[i].a[1], len(min_dat.t)),
-        resample(ad_accel[i].a[2], len(min_dat.t))
-        ]
-    )
-    ad_accel[i].t = min_dat.t
-
-for i in range(len(ad_omega)):
-    ad_omega[i].omega = np.array(resample(ad_omega[i].omega, len(min_dat.t)))
-    ad_omega[i].t = min_dat.t
 
 Plot(ad_accel, ad_omega)
