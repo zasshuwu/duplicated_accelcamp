42c57e759f94ff6908191bda0e4c7a8dd1ff64b0
Chabje1 <chabje01@gmail.com>
2020-03-02 20:03:40 -0500

    Documentation Update for Simulate.py
    added documentation for alpha/radius parameters and examples of functions for them

--- src/modules/Simulate.py
+++ src/modules/Simulate.py
@@ -2,7 +2,6 @@ from modules import Tools
 from modules.DataStructures import *
 import math
 
-
 # region Generic AlphaSim Functions
 def AlphaSim_ConstOmegaPositive():
     return
@@ -68,17 +67,20 @@ def simAlpha(N, dT, A, omega_0=0.0, noise=(0, 0)):
     :param noise: (loc, scale)
     :param N: number of iterations
     :param dT: delta t
-    :param A: alpha *can be either a float or a function*
+    :param A: alpha *can be either a number or a function that returns a number* <- example at the end of the script
     :param omega_0: initial omega value
     :return:
     """
     typeofA = 'f'
+    # This variable is a flag for the type of the variable A
+    # 'f' means that A is of type 'function'
+    # 'n' means that A is a number (float, int, double, ...)
     try:
-        A(0)
+        A(0) # if A is callable, this won't raise an Error
         typeofA = 'f'
     except:
         try:
-            A = float(A)
+            A = float(A) # if A can be converted to a float, this won't raise an Error
             typeofA = 'n'
         except:
             raise ValueError('A must be a function or a number')
@@ -86,7 +88,7 @@ def simAlpha(N, dT, A, omega_0=0.0, noise=(0, 0)):
     omega = np.array([np.double(omega_0)] * N)
     time = np.array([np.double(0.0)] * N)
     for i in range(1, N):
-        omega[i] = omega[i - 1] + (A if typeofA == 'n' else A(i * dT)) * dT
+        omega[i] = omega[i - 1] + (A if typeofA == 'n' else A(i * dT)) * dT # small check to verify the value of typeofA
         time[i] = i * dT
 
     return RotaryData(time, omega + np.random.normal(noise[0], noise[1], len(omega)))
@@ -97,16 +99,19 @@ def convertOmegaAccel(OmegaData, radius, phi=0, noise=(0, 0)):
     :param phi: angle of accelerometer
     :param noise: (loc, scale)
     :param OmegaData: data to base the accel data on
-    :param radius: radius of apparatus
+    :param radius: radius of rotation ***MUST BE EITHER A NUMBER OR A FUNCTION THAT RETURNS A NUMBER*** <- example at the end of the script
     :return:
     """
-    typeofrad = 'f'
+    typeofrad = 'f' 
+    # This variable is a flag for the type of the variable radius
+    # 'f' means that radius is of type 'function'
+    # 'n' means that radius is a number (float, int, double, ...)
     try:
-        radius(0)
+        radius(0) # if radius is callable, this won't raise an Error
         typeofrad = 'f'
     except:
         try:
-            radius = float(radius)
+            radius = float(radius) # if radius can be converted to a float, this won't raise an Error
             typeofrad = 'n'
         except:
             raise ValueError('radius must be a function or a number')
@@ -114,9 +119,10 @@ def convertOmegaAccel(OmegaData, radius, phi=0, noise=(0, 0)):
     deltaT = OmegaData.t[1] - OmegaData.t[0]
     a = []
     for i in range(len(OmegaData) - 1):
+        radius = radius if typeofrad == 'n' else radius(OmegaData.t[i]) # small check to verify the value of typeofrad
         rotated = Tools.rotate_vec3(
             [
-                OmegaData.omega[i] ** 2 * (radius if typeofrad == 'n' else radius(OmegaData.t[i])),
+                OmegaData.omega[i] ** 2 * radius, 
                 radius * (OmegaData.omega[i + 1] - OmegaData.omega[i]) / deltaT,
                 0
             ],
@@ -133,3 +139,10 @@ def convertOmegaAccel(OmegaData, radius, phi=0, noise=(0, 0)):
     ]).transpose()
     return AccelData(OmegaData.t[:-1], a, "synthetic data")
 # endregion
+
+# ======================== Example for radius and alpha functions ========================
+def example_radius(x): # the function must have only one input which is a number
+    out = 4*x
+    # stuff happens here...
+    return out # the function must return a single numerical value
+
