699289b143bce9ff37f5ea671c94da4133484cf2
Chabje1 <chabje01@gmail.com>
2020-03-09 22:11:19 -0400

    Update Optimizers.py

--- src/modules/Optimizers.py
+++ src/modules/Optimizers.py
@@ -29,7 +29,7 @@ class AdamOptimizer1D(Optimizer):
         self.configs['grad_approx'] = 0.00001
 
 
-    def Optimize(self, alpha, beta1, beta2, e=1e-8):
+    def Optimize(self, alpha, beta1, beta2, e=1e-8, return_array=False):
         m = [0]*self.configs['N']
         v = [0]*self.configs['N']
         x = [self.configs['x0']]*self.configs['N']
@@ -41,3 +41,9 @@ class AdamOptimizer1D(Optimizer):
                     self.CallFunction(x[t - 1] - self.configs['grad_approx'])
             )/(2 * self.configs['grad_approx'])
             m[t] = beta1*m[t-1] + (1 - beta1)*grad
+            v[t] = beta2 * v[t-1] + (1 - beta2) * pow(grad, 2)
+            mc = m[t]/(1 - pow(beta1, t))
+            vc = v[t]/(1 - pow(beta2, t))
+            x[t] = x[t-1] - alpha * mc / (sqrt(vc) + e)
+       self.x = x
+       return x[:-1] if not return_array else x
