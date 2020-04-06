0b3db20938d4d6a322b5effcdc15196b17038576
HA <hoanganh.theodore@icloud.com>
2020-04-06 13:53:37 -0400

    test cases for the new integration

--- src/tests/Test_Plotter_Integration.py
+++ src/tests/Test_Plotter_Integration.py
@@ -6,6 +6,14 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from modules import LoadOmega, Cluster, Simulate, Plotter_Integration
 
 
-omega = LoadOmega.Load_Omega("../../data/Dataset 2/run2/run2.omega.pasco.csv")
+omega = LoadOmega.Load_Omega("data/Dataset 2/run2/run2.omega.pasco.csv")
 accel = Simulate.AccelData_Rotate(Simulate.AccelData_CreateFromRotary(omega, random.randint(0, 10)), random.randint(0, 2))
-Plotter_Integration.Plot([accel], [omega])
+
+Plotter_Integration.MPPlot([accel], [omega])
+
+a = []
+
+for i in range(100):
+    a.append([random.randint(0,20), random.randint(0,20), random.randint(0,20)])
+
+Plotter_Integration.PPPlot(*a)
