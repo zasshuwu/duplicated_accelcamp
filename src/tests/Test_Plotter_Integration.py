d07cc9da39302c5fcab9ba1d1df8973f2e532554
HA <hoanganh.theodore@icloud.com>
2020-04-07 18:29:52 -0400

    added dialog option for windows to Test_Plotter_Integration

--- src/tests/Test_Plotter_Integration.py
+++ src/tests/Test_Plotter_Integration.py
@@ -1,12 +1,21 @@
 import os, os.path
 import sys
 import random
+import platform
+from tkinter import messagebox
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from modules import LoadOmega, Cluster, Simulate, Plotter_Integration
 
 
-omega = LoadOmega.Load_Omega("data/Dataset 2/run2/run2.omega.pasco.csv")
+
+if str(platform.system()) == 'Linux':
+    omega = LoadOmega.Load_Omega("../../data/Dataset 2/run2/run2.omega.pasco.csv")
+else:
+    messagebox.showinfo("ALERT", "Select an omega dataset from data/dataset/")
+    omega = LoadOmega.Load_Omega()
+
+
 accel = Simulate.AccelData_Rotate(Simulate.AccelData_CreateFromRotary(omega, random.randint(0, 10)), random.randint(0, 2))
 
 Plotter_Integration.MPPlot([accel], [omega])
