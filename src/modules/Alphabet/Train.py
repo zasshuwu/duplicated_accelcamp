17a9901dffe24e6d47c32eb1107678c96a252e80
JustinCardona <cardonajustin15@gmail.com>
2020-04-17 21:35:34 -0400

    j

--- src/modules/Alphabet/Train.py
+++ src/modules/Alphabet/Train.py
@@ -8,8 +8,6 @@ from sklearn.neural_network import MLPClassifier
 import cv2
 import pickle
 
-path, dirs, files = next(os.walk("./letters_mod"))
-files.sort()
 X, y = extract_training_samples('letters')
 name = input("Name your model")
 
@@ -26,19 +24,13 @@ X_test = X_test.reshape(10000,784)
 
 print("Extracted our samples and divided our training and testing data sets")
 
-mlp2 = MLPClassifier(hidden_layer_sizes=(100,100,100,100,100,), max_iter=50, alpha=1e-4,
+mlp2 = MLPClassifier(hidden_layer_sizes=(150,150,150,150,150,), max_iter=50, alpha=1e-4,
                     solver='sgd', verbose=10, tol=1e-4, random_state=1,
                     learning_rate_init=0.1)
 
 mlp2.fit(X_train, y_train)
 print("Training set score: %f" % mlp2.score(X_train, y_train))
 print("Test set score: %f" % mlp2.score(X_test, y_test))
-
-# Processes all the scanned images and adds them to the handwritten_story
-handwritten_story = []
-for i in range(len(files)):
-    img = cv2.imread("./letters_mod/" + files[i], cv2.IMREAD_GRAYSCALE)
-    handwritten_story.append(img)
 
 # Save the model
 with open( "models/" + name + ".pickle", "wb") as f:
