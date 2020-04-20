ef90a73916abc4ad611332d2aa8b2fbc4c784e2c
Chabje1 <chabje01@gmail.com>
2020-04-20 12:16:48 -0400

    Working Optimization
    #######REQUIRES np.nan######

--- src/tests/Test_Optimizer_Angle.py
+++ src/tests/Test_Optimizer_Angle.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
-import numpy as np
 from modules.Optimizers import *
 from modules.Simulate import *
 from modules.Tools import rotate_vec3
+
 
 def alphaFn(x):
     return 1
@@ -21,7 +21,8 @@ acc_data = AccelData_CreateFromRotary(
     radius=4
 )
 
-acc_data = AccelData_Rotate(acc_data, 1)
+acc_data = AccelData_Rotate(acc_data, 2)
+
 
 def cost_SimpleRotary(theta, r, a, a_next, dt):
     a = rotate_vec3(a, theta)
@@ -31,9 +32,10 @@ def cost_SimpleRotary(theta, r, a, a_next, dt):
     term3 = 2 * a[1] * np.sqrt(a[0] / r)
     return np.square(ardot - term2 - term3)
 
+
 fn = cost_SimpleRotary
 
-index = 20
+index = 30
 
 parameters = {
     'r': 4,
@@ -42,10 +44,15 @@ parameters = {
     'dt': acc_data.delta_t(index)
 }
 
+time = np.arange(-2 * np.pi, 2 * np.pi, 0.001)
+time_fn = np.array(fn(time, *list(parameters.values())))
+
+tmin = np.nanmin(time_fn)
+tmin_index = np.where(time_fn == tmin)[0][0]
+
 Adam = AdamOptimizer_1D(fn)
-Adam.config(['x0', 0.5])
+Adam.config(['x0', time[tmin_index]])
 Adam.FillParameters(*list(parameters.values()))
-Adam.SetLimits(_min=-np.pi/4, _max=np.pi/4)
 x = Adam.Optimize(alpha=0.01, beta1=0.9, beta2=0.999, return_array=True)
 
 print('Result: ' + str(x[-1]))
@@ -60,7 +67,7 @@ if out[-1] != x[-1]:
     out.append(x[-1])
 
 out = np.array(out)
-time = np.arange(out[-1] - 3, out[0] + 3, 0.001) if out[-1] < out[0] else np.arange(out[0] - 3, out[-1] + 3, 0.001)
+# time = np.arange(out[-1] - 3, out[0] + 3, 0.001) if out[-1] < out[0] else np.arange(out[0] - 3, out[-1] + 3, 0.001)
 
 plt.plot(time, fn(time, *list(parameters.values())), color='blue')
 plt.plot(out, fn(out, *list(parameters.values())), 'o', color='green')
@@ -79,7 +86,7 @@ for item in parameters:
     # plt.figtext(k, i, item + ": " + str("%.1f" % parameters[item]) + ";", color='red', fontsize="large")
     i -= 0.05
 
-plt.ylim(-1, 500)
+# plt.ylim(-1, 500)
 plt.ylabel('cost')
 plt.xlabel('angle')
 plt.show()
