31b2a17410227e1324740aafc2d562446e18141a
zasshuwu <zasshuwu@gmail.com>
2020-04-26 14:14:13 -0400

    continued debugging

--- src/tests/Test_Plotter_Integration.py
+++ src/tests/Test_Plotter_Integration.py
@@ -6,7 +6,7 @@ from tkinter import messagebox
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from modules import LoadOmega, Cluster, Simulate, Plotter_Integration
+from modules import LoadOmega, Cluster, Simulate, Plotter
 
 if str(platform.system()) == 'Linux':
     omega = LoadOmega.Load_Omega("../../data/Dataset 2/run2/run2.omega.pasco.csv")
@@ -17,14 +17,13 @@ else:
     except:
         print("No files were chosen. Exiting with code 69-420-247...")
 
-accel = Simulate.AccelData_Rotate(Simulate.AccelData_CreateFromRotary(omega, random.randint(0, 10)),
-                                  random.randint(0, 2))
+accel = Simulate.AccelData_Rotate(Simulate.AccelData_CreateFromRotary(omega, random.randint(0, 10)), random.randint(0, 2))
 
-Plotter_Integration.MPPlot([accel], [omega])
+Plotter.MultiPlotter([accel], [omega])
 
 a = []
 
 for i in range(100):
     a.append([random.randint(0, 20), random.randint(0, 20), random.randint(0, 20)])
 
-Plotter_Integration.PPPlot(*a)
+Plotter.PPPlot(*a)
